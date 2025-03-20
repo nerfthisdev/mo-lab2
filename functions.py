917f13f7eb4ba236306426671ff2a2f832672134
nerfthisdev <353054@niuitmo.ru>
2025-03-20 20:37:12 +0300

    feat: add comments in functions.py

--- functions.py
+++ functions.py
@@ -1,10 +1,13 @@
 import math
 
 def f(x):
+    """Целевая функция: f(x) = x² + e^(-x)"""
     return x**2 + math.exp(-x)
 
 def df(x):
+    """Первая производная: f'(x) = 2x - e^(-x)"""
     return 2*x - math.exp(-x)
 
 def d2f(x):
+    """Вторая производная: f''(x) = 2 + e^(-x)"""
     return 2 + math.exp(-x)
